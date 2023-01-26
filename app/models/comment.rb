8c16e59232728fd2b1f0ff05789bf48cfec02f7c
gandradep <eventsonido@gmail.com>
2023-01-26 15:41:35 -0600

    Update create custom methods for entities

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_post_comment_counter
+    post.update(commentsCounter: post.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  def update_post_counter
+    author.update(postsCounter: author.posts.count)
+  end
+
+  def recent_five_posts
+    comments.order(created_at: :desc).limit(5)
+  end
 end
