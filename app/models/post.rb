3359539f173c34703bbfdfce1ebc578ea38fcded
gandradep <eventsonido@gmail.com>
2023-01-26 17:49:59 -0600

    Validation for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
+  validates :commentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # associations
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
