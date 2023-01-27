54fcd3c5a3bbd6db5fc8700f0924ecedd65d5d08
gandradep <eventsonido@gmail.com>
2023-01-26 20:40:53 -0600

    Spec test for Like Model

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  # tests go here
+  it 'update post likesCounter' do
+    author = User.create(name: 'Tom', postsCounter: 0)
+    post = Post.create(author:, title: 'Post 1', commentsCounter: 0, likesCounter: 0)
+    Like.create(author:, post:)
+    expect(post.likesCounter).to eq(1)
+  end
 end
