e56ba36b0b8044aab996fa0130470ad23e88d8f5
gandradep <eventsonido@gmail.com>
2023-01-26 20:39:31 -0600

    Spec test for Comment model

--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # tests go here
+  it 'update post commentsCounter' do
+    author = User.create(name: 'Tom', postsCounter: 0)
+    post = Post.create(author:, title: 'Post 1', commentsCounter: 0, likesCounter: 0)
+    Comment.create(author:, post:)
+    expect(post.commentsCounter).to eq(1)
+  end
 end
