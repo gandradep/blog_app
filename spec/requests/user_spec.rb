a33563f1ba1fc50497491bb5cfab1f99c7d20eb9
gandradep <eventsonido@gmail.com>
2023-01-31 21:08:27 -0600

    Testing /users route

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :request do
+  context 'Testing /users route' do
+    before(:each) do
+      get '/users'
+    end
+    it 'response status correct' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'response status not incorrect' do
+      expect(response).not_to have_http_status(:not_found)
+    end
+    it 'response renders index mehtod' do
+      expect(response).to render_template(:index)
+    end
+    it 'response not renders show method' do
+      expect(response).not_to render_template(:show)
+    end
+    it 'response not renders show method' do
+      expect(response.body).to include('<h1>List of users</h1>')
+    end
+
+  end
+end
