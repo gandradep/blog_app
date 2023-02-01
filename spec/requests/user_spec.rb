7b9657a5b661019113a33c4443a215a6f73814c2
gandradep <eventsonido@gmail.com>
2023-01-31 21:11:33 -0600

    Testing users/:id route

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -20,6 +20,26 @@ RSpec.describe User, type: :request do
     it 'response not renders show method' do
       expect(response.body).to include('<h1>List of users</h1>')
     end
+  end
 
+  context 'Testing /users/:id route' do
+    before(:each) do
+      get '/users/1'
+    end
+    it 'response status correct' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'response status not incorrect' do
+      expect(response).not_to have_http_status(:not_found)
+    end
+    it 'response renders index mehtod' do
+      expect(response).to render_template(:show)
+    end
+    it 'response not renders show method' do
+      expect(response).not_to render_template(:index)
+    end
+    it 'response not renders show method' do
+      expect(response.body).to include('<h1>show single user</h1>')
+    end
   end
 end
