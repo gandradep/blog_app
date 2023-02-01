31d37811ab7164e40fd76b7c0f18e7a716de90c4
gandradep <eventsonido@gmail.com>
2023-01-31 21:17:00 -0600

    Test /users/:id/posts/:id route

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -21,4 +21,25 @@ RSpec.describe Post, type: :request do
       expect(response.body).to include('<h1>List of posts</h1>')
     end
   end
+
+  context 'Testing /users/:id/posts/:id route' do
+    before(:each) do
+      get '/users/1/posts/1'
+    end
+    it 'response status correct' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'response status not incorrect' do
+      expect(response).not_to have_http_status(:not_found)
+    end
+    it 'response renders index mehtod' do
+      expect(response).to render_template(:show)
+    end
+    it 'response not renders show method' do
+      expect(response).not_to render_template(:index)
+    end
+    it 'response not renders show method' do
+      expect(response.body).to include('<h1>show single post</h1>')
+    end
+  end
 end
