23c72ce887efdf0a7ce912b66b3009ab979bcff3
gandradep <eventsonido@gmail.com>
2023-02-02 13:57:54 -0600

    Create and styled /users/:id/posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  layout 'standard'
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
   def show; end
 end
