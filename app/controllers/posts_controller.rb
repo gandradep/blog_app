b7d7a2a9d1335882930ca3c8d936d97f2507bc19
gandradep <eventsonido@gmail.com>
2023-02-03 16:47:42 -0600

    Create form to create a form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,26 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(params.required(:post).permit(:title, :text))
+    post.author = current_user
+    post.likesCounter = 0
+    post.commentsCounter = 0
+    p post
+    if post.save
+      flash[:success] = 'Post saved'
+      redirect_to user_posts_url
+    else
+      flash[:error] = 'Post not created'
+      render :new
+    end
+
+  end
+
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
+
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index new create show]
   end
 end
