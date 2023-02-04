d028b56427f4a9c7bba274ba062a5c7f54705cb7
gandradep <eventsonido@gmail.com>
2023-02-04 05:45:12 -0600

    Add new view for new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,24 @@
 class CommentsController < ApplicationController
   layout 'standard'
+  def new
+    @comment = Comment.new
+  end
+
   def create
     post = Post.find(params[:post_id])
     user = User.find(params[:user_id])
-    @comment = Comment.new
     @comment = Comment.new(params.require(:comment).permit(:text))
     @comment.author = current_user
     @comment.post = post
     if @comment.save
       flash[:success] = 'Comment created succesfully!'
       p 'saved'
+      redirect_to user_post_url(user, post)
     else
       flash[:error] = "The comment couldn't be created!"
+      render :new, status: :unprocessable_entity
       p 'not save'
     end
-    redirect_to user_post_url(user, post)
+
   end
 end
