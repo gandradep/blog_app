bfd6cca766f4fc3c95b260b8acdc3f21f136e26e
gandradep <eventsonido@gmail.com>
2023-02-07 14:17:31 -0600

    add user index tests

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'User index', type: :feature do
+  before do
+    @user1 = User.create(
+      name: 'Lilly',
+      postsCounter: 0,
+      photo: 'https://unsplash.com/photos/k-1Q0eBFo_0',
+      bio: 'Testing bio'
+    )
+    @user2 = User.create(
+      name: 'Pepe',
+      postsCounter: 2,
+      photo: 'https://unsplash.com/photos/nMuIXO5EEpM',
+      bio: 'Testing bio PEPe'
+    )
+    visit root_path
+  end
+
+  it 'shows the username' do
+    expect(page).to have_content('Lilly')
+    expect(page).to have_content('Pepe')
+  end
+
+  it "When I click on a user, should redirected to that user's show page." do
+    click_on @user1.name
+    expect(page).to have_current_path user_path(@user1.id)
+  end
+end
