f629c08bb4dbd62b31b7464430d44ff051912f25
gandradep <eventsonido@gmail.com>
2023-02-07 17:20:08 -0600

    Fix linters

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,20 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 User.create([
-  { name: 'User1', photo: 'https://example.com/user1.jpg', bio: 'Bio1' },
-  { name: 'User2', photo: 'https://example.com/user2.jpg', bio: 'Bio2' },
-  { name: 'User3', photo: 'https://example.com/user3.jpg', bio: 'Bio3' },
-  { name: 'User4', photo: 'https://example.com/user4.jpg', bio: 'Bio4' },
-  { name: 'User5', photo: 'https://example.com/user5.jpg', bio: 'Bio5' }
-])
+              { name: 'User1', photo: 'https://example.com/user1.jpg', bio: 'Bio1' },
+              { name: 'User2', photo: 'https://example.com/user2.jpg', bio: 'Bio2' },
+              { name: 'User3', photo: 'https://example.com/user3.jpg', bio: 'Bio3' },
+              { name: 'User4', photo: 'https://example.com/user4.jpg', bio: 'Bio4' },
+              { name: 'User5', photo: 'https://example.com/user5.jpg', bio: 'Bio5' }
+            ])
 
 users = User.all
 
 posts = []
 5.times do |i|
   posts << Post.create(
-    title: "Post #{i+1}",
-    text: "Text #{i+1}",
+    title: "Post #{i + 1}",
+    text: "Text #{i + 1}",
     author: users[i % 5]
   )
 end
@@ -28,7 +28,7 @@ end
 comments = []
 5.times do |i|
   comments << Comment.create(
-    text: "Comment #{i+1}",
+    text: "Comment #{i + 1}",
     author: users[i % 5],
     post: posts[i % 5]
   )
