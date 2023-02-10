b8bfb7d436e5712b204bdabf86ee4e5487faa5a4
amare1990 <amaremek@gmail.com>
2023-02-10 22:29:44 +0300

    Add api route

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,13 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end
+
+  namespace :api do
+    resources :users, only: %i[index show] do
+      resources :posts, only: %i[index show] do
+        resources :comments, only: %i[index create]
+      end
+    end
+  end
+
 end
