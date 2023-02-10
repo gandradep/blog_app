793e812d7e18335f29f76c6f1d31d8bdef1e2beb
amare1990 <amaremek@gmail.com>
2023-02-10 22:35:02 +0300

    Add api PostsController method

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,12 @@
 class Api::PostsController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    posts = user.posts
+    render json: posts, status: :ok
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post, status: :ok
+  end
 end
